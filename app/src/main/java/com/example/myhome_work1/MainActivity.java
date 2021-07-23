d541862afca2faad06ae1e07fc2cffe828c8b98d
Akimov <akimov1986@internet.ru>
2021-07-23 18:54:48 +0700

    navigate with a button

--- app/src/main/java/com/example/myhome_work1/MainActivity.java
+++ app/src/main/java/com/example/myhome_work1/MainActivity.java
@@ -1,33 +1,33 @@
 package com.example.myhome_work1;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.CalendarView;
-import android.widget.Toast;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
 
+        Button btn = (Button) findViewById(R.id.button1);
+        btn.setOnClickListener(this);
+    }
 
-        CalendarView calendarView = findViewById(R.id.calendarView);
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                Intent intent = new Intent(getApplicationContext(), MainActivitySecond.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
 
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year,
-                                            int month, int dayOfMonth) {
-                int mYear = year;
-                int mMonth = month;
-                int mDay = dayOfMonth;
-                String selectedDate = new StringBuilder().append(mMonth + 1)
-                        .append("-").append(mDay).append("-").append(mYear)
-                        .append(" ").toString();
-                Toast.makeText(getApplicationContext(), selectedDate, Toast.LENGTH_LONG).show();
-            }
-        });
     }
 }
